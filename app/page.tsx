0340d7ad0936767854d37a0593c3509e7683ae53
ShreyashBRN <shreyashbrn@gmail.com>
2026-08-08 14:40:51 +0530

    test: responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Collaborate from "@/components/Collaborate";
-import Contact from "@/components/Contact";
+// import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
